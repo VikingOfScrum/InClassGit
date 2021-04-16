440462e8825e120f625601be196a8e11030d2579
hanson-joey-7595 <49300264+hanson-joey-7595@users.noreply.github.com>
2021-04-15 17:46:14 -0700

    added master list, random choice, variable, temp pw

--- passwordGen.py
+++ passwordGen.py
@@ -6,4 +6,13 @@ def createPassword(length):
     digitsAndSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
     lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    masterList = digitsAndSymbols + lowerCaseChars + upperCaseChars
+    
+    randDigitSymbol = random.choice(digitsAndSymbols)
+    randLowerChar = random.choice(lowerCaseChars)
+    randUpperChar = random.choice(upperCaseChars)
+    
+    createdPassword = ""
+    tempPassword = randDigitSymbol + randLowerChar + randUpperChar
 createPassword(12)
